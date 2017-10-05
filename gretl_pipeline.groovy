ff2211e5936923540616ca9015da4dfc3486e6e8
bjsvwjek <jekero@outlook.com>
2017-10-05 13:40:59 +0200

    Added parameters to gretlpipe / removed remaining absolute path in one integration test

--- gretl_pipeline.groovy
+++ gretl_pipeline.groovy
@@ -13,7 +13,7 @@ on the same tools
 - Rethink the versioning as it is overly complex for it's limited benefit
 - Refactor the pipeline to result in docker image for the gretl runtime
  and finally in the released runtime with job configuration
- 
+
  questions:
  - Stages snapshot jar and integration test share the same environment variables.
  Can these be declared only once?
@@ -21,15 +21,21 @@ on the same tools
  How can the block be reused in declarative syntax?
  - The workflow to set version and jar url in init.gradle is a bit "hacky".
  Better alternatives?
+ - How to add simple if statements in declarative pipe (for AVOID_PROD_PUBLICATION)?
 */
 pipeline {
     agent any
+    parameters {
+        string(name: 'PUBLISH_USER_PASS', description: 'What is the publish user and password (Write access for the AIO Maven Repository)?')
+        string(name: 'GRETL_VERSION_NUM', description: 'What Version number should be deployed? Example: 1.0.2')
+        string(name: 'AVOID_PROD_PUBLICATION', defaultValue: 'yes', description: 'Any non empty string causes the pipeline to fail and stop before the production publication')
+    }
     environment{
         GRADLE_HOME = '/opt/gradle/gradle-3.5.1/bin/'
-        GRETL_JAR_USER = 'agipublisher'
-        GRETL_JAR_PASS = 'agipublisher'
+        GRETL_JAR_USER = "${params.PUBLISH_USER_PASS}"
+        GRETL_JAR_PASS = "${params.PUBLISH_USER_PASS}"
         GRETL_JAR_BASEURL = 'https://artifactory.verw.rootso.org/artifactory/repo-agi-local'
-        GRETL_VERSION_NUMBER = '1.0.2'
+        GRETL_VERSION_NUMBER = "${params.GRETL_VERSION_NUM}"
     }
     stages {
         stage('Gradle Build') {
@@ -37,7 +43,7 @@ pipeline {
                 git 'https://github.com/uncoyote/gretlv1.git'
 
                 // Run the gradle build
-                sh GRADLE_HOME + "gradle compilet"
+                sh GRADLE_HOME + "gradle build"
             }
         }
         stage('Snapshot Jar') {
@@ -71,10 +77,10 @@ pipeline {
         stage('Release Jar') {
             environment {
                 GRETL_VERSION = "$GRETL_VERSION_NUMBER"
-                GRETL_JAR_URL = "$GRETL_JAR_BASEURL" + '/FailsWithThisDummyUrl'
+                GRETL_JAR_URL = "$GRETL_JAR_BASEURL"
             }
             steps { //publish to release
-                sh GRADLE_HOME + "gradle publish"
+                sh GRADLE_HOME + "${params.AVOID_PROD_PUBLICATION}" + "gradle publish"
 
                 dir('./itest/integrationtests'){
                     sh "cp inittemplate.txt init.gradle"
